f508e908b993e013a8e5e0c85a19f3e334df4a77
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-18 20:49:23 -0700

    custom _document with polaris css

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import NextDocument, {Head, Main, NextScript} from 'next/document'
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="https://sdks.shopifycdn.com/polaris/2.12.1/polaris.min.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
