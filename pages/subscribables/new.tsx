d296a5d5cc1befb436670e0a745e98fb1bdfc71d
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-26 08:37:56 -0700

    fetching newSubscribableOptions

--- pages/subscribables/new.tsx
+++ pages/subscribables/new.tsx
@@ -1,8 +1,18 @@
 import {Button, Form, FormLayout, Heading, Page, TextField} from '@shopify/polaris'
+import gql from 'graphql-tag'
 import {Component} from 'react'
+import {Query} from 'react-apollo'
 
 import Header from '../../components/header'
 import {handleInputChangeToStateUpdate} from '../../lib/update-state'
+
+const QUERY = gql`
+  query newSubscribableOptions {
+    newSubscribableOptions {
+      types
+    }
+  }
+`
 
 export default class NewSubscribable extends Component<{}> {
   public state = {
@@ -12,18 +22,26 @@ export default class NewSubscribable extends Component<{}> {
   private handleInputChangeToStateUpdate = handleInputChangeToStateUpdate.bind(this)
 
   public render() {
-    return <Page title='New Subscribable'>
-      <Header />
+    return <Query query={QUERY}>
+      {({data, loading}) => {
+        if (loading) { return <div>Loading...</div> }
 
-      <Heading>New Subscribable</Heading>
+        console.log(data)
 
-      <Form onSubmit={this.handleSubmit}>
-        <FormLayout>
-          <TextField label='name' onChange={this.handleInputChangeToStateUpdate('nameInput')} type='text' value={this.state.nameInput} />
-        </FormLayout>
-        <Button submit>Submit</Button>
-      </Form>
-    </Page>
+        return <Page title='New Subscribable'>
+          <Header />
+
+          <Heading>New Subscribable</Heading>
+
+          <Form onSubmit={this.handleSubmit}>
+            <FormLayout>
+              <TextField label='name' onChange={this.handleInputChangeToStateUpdate('nameInput')} type='text' value={this.state.nameInput} />
+            </FormLayout>
+            <Button submit>Submit</Button>
+          </Form>
+        </Page>
+      }}
+    </Query>
   }
 
   private handleSubmit = (e: React.FormEvent) => {
