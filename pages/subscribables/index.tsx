8350d4d2c8cf58c6078c794f03738067bc6c6960
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-30 06:12:41 -0700

    loading subscribables

--- pages/subscribables/index.tsx
+++ pages/subscribables/index.tsx
@@ -1,8 +1,32 @@
 import {Link, Page} from '@shopify/polaris'
+import gql from 'graphql-tag'
+import {Query} from 'react-apollo'
 
 import Header from '../../components/header'
 
+const QUERY = gql`
+  query subscribables {
+    subscribables {
+      id
+      products {
+        shopifyProductId
+      }
+      sizes {
+        id
+        numVariants
+        price
+      }
+    }
+  }
+`
+
 export default () => <Page title='Subscribables'>
   <Header />
+  <Query query={QUERY}>
+    {({data, loading}) => {
+      if (loading) { return <div>Loading...</div> }
+      return <div>You have {data.subscribables.length} subscribables.</div>
+    }}
+  </Query>
   <Link url='/subscribables/new'>New</Link>
 </Page>
